484269f8ea66b445efa39169759ad341fc5c075c
Starbow-Break <phsyobs@naver.com>
2025-12-07 17:59:08 +0900

    NTT 및 정수 거듭제곱 추가

---
+++ Math/MathI.cs
@@ -0,0 +1,23 @@
+﻿namespace ProblemSolving.Math;
+
+public static class MathI
+{
+    public static int ModPow(int a, int e, int mod)
+    {
+        int result = 1;
+        int w = a;
+
+        while (e > 0)
+        {
+            if (e % 2 == 1)
+            {
+                result = (int)(1L * result * w % mod);
+            }
+            
+            w = (int)(1L * w * w % mod);
+            e >>= 1;
+        }
+
+        return result;
+    }
+}
---
+++ Math/NTT.cs
@@ -0,0 +1,62 @@
+﻿namespace ProblemSolving.Math;
+
+public static class NTT
+{
+    public const int Mod = 998244353;
+    private const int W = 3;
+
+    public static int[] FFT(int[] poly, bool inverse = false)
+    {
+        int polySize = 1;
+        while (polySize < poly.Length)
+        {
+            polySize *= 2;
+        }
+        
+        int unit = MathI.ModPow(W, (Mod - 1) / polySize, Mod);
+        if (inverse)
+        {
+            unit = MathI.ModPow(unit, Mod - 2, Mod);
+        }
+        
+        int[] p = new int[polySize];
+        for (int i = 0; i < polySize; i++)
+        {
+            int idx = 0;
+            for (int j = 1, a = polySize / 2; j < polySize; j <<= 1, a >>= 1)
+            {
+                if ((i & j) > 0)
+                {
+                    idx += a;
+                }
+            }
+            
+            p[i] = (idx >= poly.Length ? 0 : poly[idx]);
+        }
+
+        for (int s = 2; s <= polySize; s *= 2)
+        {
+            int w = MathI.ModPow(unit, polySize / s, Mod);
+            for (int i = 0; i < polySize; i += s)
+            {
+                int curW = 1;
+                for (int j = i; j < i + s / 2; j++, curW = (int)(1L * curW * w % Mod))
+                {
+                    int te = p[j];
+                    int to = p[j + s / 2];
+
+                    p[j] = (int)((1L * curW * to + te) % Mod);
+                    p[j + s / 2] = (int)((1L * (Mod - curW) * to + te) % Mod);
+                }
+            }
+        }
+
+        if (inverse)
+        {
+            int invSize = MathI.ModPow(polySize, Mod - 2, Mod);
+            p = p.Select(x => (int)(1L * x * invSize % Mod)).ToArray();
+        }
+        
+        return p;
+    }
+}
